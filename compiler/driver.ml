536a303961684122742c09958ae98a5a0f5f24de
Pierre Goutagny <pierre.goutagny@inria.fr>
2025-02-28 15:04:23 +0100

    Revert "Print interpreter time"
    This reverts commit 9ba1ee12855daad8b60c093cdb6d4a7e0c5c9004.

--- compiler/driver.ml
+++ compiler/driver.ml
@@ -685,13 +685,9 @@ module Commands = struct
     let prg, _ =
       Passes.dcalc options ~includes ~optimize ~check_invariants ~typed
     in
-    let t1 = Sys.time () in
     Interpreter.load_runtime_modules prg;
-    let r = print_interpretation_results options Interpreter.interpret_program_dcalc prg
+    print_interpretation_results options Interpreter.interpret_program_dcalc prg
       (get_scope_uid prg.decl_ctx ex_scope)
-    in 
-    Message.result "interpret time %f" (Sys.time () -. t1);
-    r
 
   let lcalc
       typed
